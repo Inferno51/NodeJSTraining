f6610149ce5f3660561cb523c36ebe365317c27b
Jonathan Pack <jon.pack@me.com>
2017-11-28 11:09:58 -0500

    Updated time lab example

--- Labs/Ch02-NPM/Begin/timeExample.js
+++ Labs/Ch02-NPM/Begin/timeExample.js
@@ -1,3 +1,7 @@
 var moment = require('moment');
+var moment_timezone = require('moment-timezone');
+var current = moment();
 
 console.log(moment("01012011", "MM-DD-YYYY").fromNow());
+console.log(current.tz('America/New_York').format('ha z'));
+console.log(current.tz('America/Los_Angeles').format('ha z'));
